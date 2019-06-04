06e13a32a95f4834ff660c4fd6bed59107c505b1
AASHISH UPADHYAY <42029355+aashish31f@users.noreply.github.com>
2019-06-04 00:09:57 -0700

    Updating review classifier

--- review_classifier.py
+++ review_classifier.py
@@ -3,7 +3,7 @@
 # Importing libraries
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt 
+
 
 # Importing dataset
 dataset = pd.read_csv('Restaurant_Reviews.tsv',delimiter='\t',quoting=3)
